fb78abb79c915b72909309fd3d5f20cdffb84144
Hitesh Gupta <hitesh.gupta@ditstek.com>
2021-11-09 09:36:38 +0530

    update code for home page

--- common/commonmeta.php
+++ common/commonmeta.php
@@ -1,6 +1,9 @@
 <?php
   $current_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
   $current_page = str_replace($url, "", $current_url); 
+  if($current_page == ""){
+    $current_page = "/";
+  }
   $res = mysqli_query($conn, "select * from meta_tags where slug='$current_page'");
   $check = mysqli_num_rows($res);
   if ($check > 0) {
