215b5b593aa56ac1b4950387e545be1707e6b014
neha <nehathakurwins@gmail.com>
2022-12-13 18:00:58 +0530

    UI Fixes

--- industries/business-and-workflow-automation.php
+++ industries/business-and-workflow-automation.php
@@ -47,22 +47,19 @@ require_once('../core/dbconnection.php');
                             <img class="rightImage" src="../assets/domains/business-automation.png" alt="image" />
                             <h2 class="pb-1">Business And Workflow Automation</h2>
                             <p><strong>Business And Workflow Automation</strong> is the automation of business processes through technology, empowering businesses to curtail expenses and enhance productivity. Rather than hiring employees doing repeated and menial jobs, the businesses can let the software handle these tasks. <strong>Approval management, Employee onboarding, Customer support,</strong> evaluation of examinations are examples of business process automation. </p>
-                            
-                        </div>
-                        <div class="innerThumbDesc">
-                             <h3 class="pt-1">Here are some of the domains which are reaping the rewards of business process automation:</h3>
+                            <h3 class="pt-1">Here are some of the domains which are reaping the rewards of business process automation:</h3>
                             <ul class="buisnessDomains">
-                                <li><a href="javascript:void(0);">Supply Chain</a></li>
-                                <li><a href="javascript:void(0);">HR</a></li>
-                                <li><a href="javascript:void(0);">Financial & Legal</a></li>
-                                <li><a href="javascript:void(0);">Management Procurement</a></li>
-                                <li><a href="javascript:void(0);">TeleHealth and TeleMedicine</a></li>
-                                <li><a href="javascript:void(0);">Sales & Marketing</a></li>
-                                <li><a href="javascript:void(0);">Production </a></li>
-                                <li><a href="javascript:void(0);">Banking</a></li>
-                                <li><a href="javascript:void(0);">Finance</a></li>
-                                <li><a href="javascript:void(0);">Insurance</a></li>
-                                <li><a href="javascript:void(0);">Education</a></li>
+                                <li><a href="#">Supply Chain</a></li>
+                                <li><a href="#">HR</a></li>
+                                <li><a href="#">Financial & Legal</a></li>
+                                <li><a href="#">Management Procurement</a></li>
+                                <li><a href="#">TeleHealth and TeleMedicine</a></li>
+                                <li><a href="#">Sales & Marketing</a></li>
+                                <li><a href="#">Production </a></li>
+                                <li><a href="#">Banking</a></li>
+                                <li><a href="#">Finance</a></li>
+                                <li><a href="#">Insurance</a></li>
+                                <li><a href="#">Education</a></li>
                             </ul>
                             <p><strong>DITS</strong> provides <strong>business automation applications outsourcing services</strong> to <strong>startups </strong> and <strong>business automation product development companies.</strong> We understand the technology, business, and automation domain and experience delivering business automation products. We also provide business automation application outsourcing services, business automation web application development services and business automation application developers for hire.</p>
                             <p class="pt-2">If you are a <strong>startup</strong> or an <strong>enterprise-level company</strong> that requires automating your business process, we can help you with <strong>Mobile Application Development, Web Application Development, API Integration, Quality Assurance</strong> or any other custom requirement.</p>
